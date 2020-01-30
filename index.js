77e56fa631787a0967ccb13950f47e60adf05de4
190968 <39701783+190968@users.noreply.github.com>
2020-01-30 11:24:51 +0300

    new

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get("/bob", function(req,res) {
      
       
   
-const port = process.env.PORT || 5000;
 
-http.createServer(app).listen(port, function(){console.log("Server running on 5000 port")});
+
+http.createServer(app).listen({port: process.env.PORT || 500});
