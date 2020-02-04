c8035cfdcb3aad66ce5ca5933dc1b988f78cf597
190968 <19ham09@gmail.com>
2020-02-04 15:25:43 +0300

    my commit

--- index.js
+++ index.js
@@ -1,88 +1,100 @@
 var express = require("express");
 var fs = require("fs");
 var http = require("http");
+var path = require("path");
 var cors = require("cors");
 var app = express();
+var formidable = require("formidable");
 app.use(cors());
 
-var port = process.env.PORT||5000;
+const port = process.env.PORT||5000;
 
 app.get("/",function(req,res) {
+  let url = req.protocol +'://' + req.hostname + ":" + `${port}`;
  
-   let s =  fs.readdirSync((__dirname + "/items"));
+  let s =  fs.readdirSync((__dirname + "/items")); 
   
-   console.log(s);
    
-    res.send(`<!doctype html>
+  res.send(`<!doctype html>
     <html>
     <head>
-    <style>
-      a {
-        box-shadow:inset 0 0 5px 5px #ddd; 
-        margin: 20px;      
-        display: inline-block;
-        padding: 5px 20px;
-        border: 2px solid #000;
-        border-radius: 80px;
-        font: 400 40px/45px "Arial",sans-serif;
-        text-decoration: none;
-        color: #000;
-      }
-      a:hover {
-        background-color: #000;
-        color: #fff;
-      }
-      @media screen and (max-width: 480px) {
+      <title>my Site</title>
+      <style>
         a {
-          font: 400 40px/40px "Arial",sans-serif;
+          box-shadow:inset 0 0 5px 5px #ddd; 
+          margin: 20px;      
+          display: inline-block;
+          padding: 5px 20px;
+          border: 2px solid #000;
+          border-radius: 80px;
+          font: 400 40px/45px "Arial",sans-serif;
+          text-decoration: none;
+          color: #000;
         }
-      }
-    </style>
+        a:hover {
+          background-color: #000;
+          color: #fff;
+        }
+        @media screen and (max-width: 480px) {
+          a {
+            font: 400 40px/40px "Arial",sans-serif;
+          }
+        }
+        div,
+        body {
+          background-color: #00ffff;
+        }
+      </style>
+    
     </head>
       <body>
         <div>
+       
           <h1 id="one" align="center">
-            <a href = http://localhost:5000/items/?brand=${s[0]}>${s[0]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[1]}>${s[1]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[2]}>${s[2]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[3]}>${s[3]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[4]}>${s[4]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[5]}>${s[5]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[6]}>${s[6]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[7]}>${s[7]}</a>
-            <a href = http://localhost:5000/items/?brand=${s[8]}>${s[8]}</a>
-            
+            <a href = ${url}/items/?brand=${s[0]}>${s[0]}</a>
+            <a href = ${url}/items/?brand=${s[1]}>${s[1]}</a>
+            <a href = ${url}/items/?brand=${s[2]}>${s[2]}</a>
+            <a href = ${url}/items/?brand=${s[3]}>${s[3]}</a>
+            <a href = ${url}/items/?brand=${s[4]}>${s[4]}</a>
+            <a href = ${url}/items/?brand=${s[5]}>${s[5]}</a>
+            <a href = ${url}/items/?brand=${s[6]}>${s[6]}</a>
+            <a href = ${url}/items/?brand=${s[7]}>${s[7]}</a>
+            <a href = ${url}/items/?brand=${s[8]}>${s[8]}</a>
+            <a>add</a>
           </h1>    
-      </div>   
-    </body>
+      </div>
+
+    </body>   
     </html>
   `);
    
 });
 
 app.get("/items",function(req,res) { 
+  let url = req.protocol +'://' + req.hostname + ":" + `${port}`;
   let brand = req.query.brand;
   let s =  fs.readdirSync((__dirname + `/items/${brand}`));
   let num = req.query.number || 0;
   let n = num < 0 ? 0 : num > s.length-1 ? s.length-1 : num; 
-  let im = s[n].replace(".jpg","");   
+  let im = s[n].replace(".jpg","").replace(".webp","");   
   res.send(`<!doctype html>
     <html>
     <head>
     <style>
       a {
-        box-shadow:inset 0 0 5px 5px #ddd;        
+        box-shadow:inset 0 0 5px 5px green;        
         display: inline-block;
-        padding: 5px 10px;
-        border: 1px solid #000;
-        border-radius: 80px;
-        font: 400 30px/30px "Arial",sans-serif;
+        padding: 10px 20px;
+        border: 2px solid #red;
+        border-radius: 90px;
+        font: 400 20px/30px "Arial",sans-serif;
         text-decoration: none;
         color: #000;
       }
       a:hover {
         background-color: #000;
         color: #fff;
+        transition: all 0.5s;
       }
       b {
         display: inline-block;
@@ -100,15 +112,19 @@ app.get("/items",function(req,res) {
     <body>
     <div>
     
-      <a href = http://localhost:5000/>main</a>
+      <a href = ${url}/>EXIT</a>
+      <a href = ${url}/upload/?brand=${brand}>UPLOAD IMAGE</a>
       <p align="center" >
-        <a  href = http://localhost:5000/items/?brand=${brand}&number=${+n-1}>prev</a>
-        <b >${im}</b>
-        <a  href = http://localhost:5000/items/?brand=${brand}&number=${+n + 1}>next</a>
+        <a  href = ${url}/items/?brand=${brand}&number=${+n-1}>prev</a>       
+        <a  href = ${url}/items/?brand=${brand}&number=${+n + 1}>next</a>
       </p>
-      <img src = http://localhost:5000/items/${brand}/${im} 
-        style="margin:0 auto;display:block;width: 80vw"
+      <p align="center">     
+        <b>${im}</b>     
+      </p>
+      <img src = ${url}/items/${brand}/${im} 
+        style="margin:0 auto;display:block"
       >
+     
     
    </div>
    
@@ -118,19 +134,92 @@ app.get("/items",function(req,res) {
    
 });
 app.all("/items/*", function(req,res,err) {
-  var path = req.path;
-  res.sendFile(__dirname +  path + ".jpg"); 
- 
+    var path = req.path;
+    let url = req.protocol +'://' + req.hostname + ":" + `${port}`;
+    let s = fs.existsSync(__dirname +  path + ".jpg");  
+    s ? 
+    res.sendFile(__dirname +  path + ".jpg"):   
+    res.sendFile(__dirname +  path + ".webp"); 
+   
 });
 
 
-app.get("/bob", function(req,res) {    
+app.get("/base", function(req,res) {    
      fs.readFile("base.json", function(err,data) { 
-         if (err) throw err;
-       
+         if (err) throw err;       
         res.send(data);
     });
-});    
+});
+
+app.get("/upload",function(req,res){ 
+  let url = req.protocol +'://' + req.hostname + ":" + `${port}`;
+  let brand = req.query.brand; 
+  res.send(`
+  <!doctype html>
+  <html>
+  <head>
+  <style>
+    input,a {
+      box-shadow:inset 0 0 5px 5px green;        
+      display: block;
+      padding: 10px 20px;
+      border: 2px solid #red;
+      border-radius: 90px;
+      margin: 20px auto;
+      font: 400 20px/30px "Arial",sans-serif;
+      cursor: pointer;
+      color: #000;
+    }
+    a {
+      width: 100px;
+      text-align: center;
+      text-decoration: none;
+    }
+    input:hover,
+    a:hover {
+      background-color: #000;
+      color: #fff;
+      transition: all 0.5s;
+    }
+   
+    @media screen and (max-width: 480px) {
+      input {
+        font: 400 40px/40px "Arial",sans-serif;
+      }
+    }
+  </style>
+  </head>
+   
+  <body>
+    <div>
+      <form action="fileupload/?brand=${brand}" method="post" enctype="multipart/form-data">
+        <input type="file" name="filetoupload">
+        <input type="submit">
+      </form>
+      <a href = ${url}/>EXIT</a>
+    </div>
+  </body>
+  </html>  
+    
+  `)
+}); 
+app.post("/upload/fileupload",function(req,res){ 
+  let url = req.protocol +'://' + req.hostname + ":" + `${port}`;
+  let brand = req.query.brand;  
+  var form = new formidable.IncomingForm();
+  form.parse(req, function (err, fields, files) {
+    var oldpath = files.filetoupload.path;
+    var newpath = path.join( __dirname,'items',`${brand}`,files.filetoupload.name);
+    fs.rename(oldpath, newpath, function (err) {
+      if (err) throw err;
+      res.write(`
+        <h1>File uploaded to ${brand} directory 
+        <a href = ${url}/>EXIT</a></h1>
+      `);
+      res.end();
+    });
+  });  
+});       
       
   
 
