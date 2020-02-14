08b834f21c71733e7c8336c552b0e061da0dd796
190968 <19ham09@gmail.com>
2020-02-14 13:40:35 +0300

    My commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(function(req, res, next) {
 
 
 const port = process.env.PORT||5000;
-
+//  Main page
 app.get("/",function(req,res) {
   app.locals.url =  port == 5000 ? req.protocol +'://' + req.hostname +  `:${port}`:
                   req.protocol +'://' + req.hostname;
@@ -31,85 +31,9 @@ app.get("/",function(req,res) {
  
   let s =  fs.readdirSync((__dirname + "/items")); 
   
-  //  Main page
-  res.send(`<!doctype html>
-    <html>
-    <head>
-      <title>my Site</title>
-      <style>
-        .folders a {
-          box-shadow:inset 0 0 5px 5px #ddd; 
-          margin: 20px;
-          width: 200px;
-          height: 50px;      
-          display: inline-block;
-         
-          border: 2px solid #000;
-         
-          font: 400 30px/50px "Arial",sans-serif;
-          text-decoration: none;
-          color: #000;
-        }
-        .add_folder {
-          box-shadow:inset 0 0 5px 5px silver; 
-          margin: 20px;      
-          display: block;
-          padding: 5px 20px;
-          border: 2px solid red;
-          width: 200px;
-          text-align: center;
-          font: 400 20px/25px "Arial",sans-serif;
-          text-decoration: none;
-          color: #000;
-        }
-        a:hover {
-          background-color: #000;
-          color: #fff;
-        }
-        div.folders {
-          margin: 5vw auto;
-          width: 60%;
-          
-        }
-        @media only screen and (max-device-width: 480px) {
-          a {
-            padding: 15px 30px;
-            font: 400 50px/50px "Arial",sans-serif;
-          }
-         
-
-        }
-        div,
-        body {
-          background-color: #00ffff;
-        }
-      </style>
-    
-    </head>
-      <body>
-        <div>
-          <a class="add_folder" href = ${app.locals.url}/createFolder>create new folder</a>
-          <a class="add_folder" href = ${app.locals.url}/upload_base>add base</a>      
-          <div class="folders" align="center">
-            <a href = ${app.locals.url}/items/?brand=${s[0]}>${s[0]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[1]}>${s[1]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[2]}>${s[2]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[3]}>${s[3]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[4]}>${s[4]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[5]}>${s[5]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[6]}>${s[6]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[7]}>${s[7]}</a>
-            <a href = ${app.locals.url}/items/?brand=${s[8]}>${s[8]}</a>
-           
-          </div>
-         
-      </div>
-
-    </body>   
-    </html>
-  `);
-   
-});
+ 
+  res.render('main_page',{ folders: s, url: app.locals.url});
+});  
 
 //pug CreateFolder
 app.get("/createFolder",(req,res)=>{
@@ -133,8 +57,9 @@ app.get("/createFolder",(req,res)=>{
 
 // pug form  for upload image
 app.get("/upload_image",(req,res)=>{ 
-  app.locals.brand = req.query.brand;  
-  res.render('image',{exit: app.locals.url, brand: app.locals.brand} )
+  app.locals.brand = req.query.brand; 
+  let s =  fs.readdirSync((__dirname + "/items"));  
+  res.render('image',{exit: app.locals.url, brand: app.locals.brand, folders: s} )
 });  
 
 
@@ -189,7 +114,7 @@ app.post("/uploadBase",(req,res)=>{
 // pug form  for upload base
 app.get("/upload_base",(req,res)=>{ 
   
-  res.render('base',{exit: app.locals.url} )
+  res.render('upload_base',{exit: app.locals.url} )
 });  
 
 
@@ -204,92 +129,12 @@ app.get("/items",function(req,res) {
   let n = num < 0 ? 0 : num > s.length-1 ? s.length-1 : num;
   let to_del = s[n]; 
   let im = s.length !== 0 ? s[n].replace(".jpg","").replace(".webp","") : s[1];   
-  res.send(`<!doctype html>
-    <html>
-    <head>
-    <style>
-      body {
-        background-color:  #00ffff;
-      }
-      a {
-        box-shadow:inset 0 0 10px 2px green;        
-        display: inline-block;
-        padding: 10px 20px;
-        border: 2px solid #red;
-        vertical-align: middle;
-       
-        font: 400 20px/30px "Arial",sans-serif;
-        text-decoration: none;
-        color: #000;
-        margin: 10px;
-      }
-      a:hover {
-        background-color: #000;
-        color: #fff;
-        transition: all 0.5s;
-      }
-      b {
-        display: inline-block;
-        width: auto;
-        font-size: 30px;
-      }
-      img {
-        display: inline-block;
-        
-        width: 50%;
-        vertical-align: middle;
-      }
-      a.delete {
-        visibility: hidden;
-      }
-      .del_block {
-        cursor: pointer;
-      }
-      p:hover a.delete {
-        transition: all 1s;
-        visibility: visible;
-      }
-      @media (max-device-width: 480px) {
-        a {
-          padding: 15px 30px;
-          font: 400 40px/40px "Arial",sans-serif;
-        }
-        img {
-          margin-top: 10vw;
-          width: 100%;
-        }
-      }
-    </style>
-    </head>
-     
-    <body>
-    <div>
-      <p align="center" >
-        <a href = ${app.locals.url}/>EXIT</a>
-        <a href = ${app.locals.url}/upload_image/?brand=${brand}>UPLOAD IMAGE</a>     
-       
-        <b>${im}</b>           
-        <a>delete file</a>
-        <a  class = "delete" href = ${app.locals.url}/items/?brand=${brand}&number=${n}>no</a> 
-        <a  class = "delete" href = ${app.locals.url}/delete/?brand=${brand}&name=${to_del}>yes</a>
-      </p>
-      <p align="center">
-        <a  href = ${app.locals.url}/items/?brand=${brand}&number=${+n-1}>prev</a>
-        <img src = ${app.locals.url}/items/${brand}/${im} alt ="no image">
-        <a  href = ${app.locals.url}/items/?brand=${brand}&number=${+n + 1}>next</a>
-      </p>
-      <p align="center"> 
-
-      </p>
-     
+  res.render('view_image',{ url: app.locals.url, brand: brand, im: im,n: n});
+});  
     
-   </div>
+  
    
-   </body>
-   </html>
-    `);
-   
-});
+
 
 //  request for send files to site
 app.all("/items/*", function(req,res,err) {
