d173cf7d9703ee158e8f695f252a9c9820beb19f
190968 <19ham09@gmail.com>
2020-02-19 12:42:23 +0300

    Commit 19.02

--- index.js
+++ index.js
@@ -24,102 +24,14 @@ app.use(function(req, res, next) {
 
 
 
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
 
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), listFiles);
-});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getAccessToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Lists the names and IDs of up to 10 files.
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listFiles(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name}`);
-      });
-    } else {
-      console.log('No files found.');
-    }
-  });
-}
 
 const port = process.env.PORT||5000;
 //  Main page
 app.get("/",function(req,res) {
   app.locals.url =  port == 5000 ? req.protocol +'://' + req.hostname +  `:${port}`:
-                  req.protocol +'://' + req.hostname;
-
- 
- 
- 
+    req.protocol +'://' + req.hostname; 
   let s =  fs.readdirSync((__dirname + "/items")); 
-  
- 
   res.render('main_page',{ folders: s, url: app.locals.url});
 });  
 
@@ -131,15 +43,11 @@ app.get("/createFolder",(req,res)=>{
   } else {
     fs.mkdir((__dirname + `/items/${name}`),function(err){
       if (err) {
-
         res.send(`<h1>${err}</h1>`)
-
       } else {
         res.redirect("/");
-      }  
-     
+      }     
     })
-
   }
 });
 
@@ -156,8 +64,7 @@ app.post("/upload_image/uploadimage",(req,res)=>{
   let brand = req.query.brand;
   console.log(brand);   
   var form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-   
+  form.parse(req, function (err, fields, files) {   
     var oldpath = files.imageupload.path;
     var newpath = path.join( __dirname,'items',app.locals.brand,files.imageupload.name);
 
@@ -172,6 +79,20 @@ app.post("/upload_image/uploadimage",(req,res)=>{
   });
 
 });
+
+// Rename folder
+
+app.get("/rename",(req,res)=>{
+  let name = req.query.name;
+  let last_name = req.query.last_name;
+   fs.rename(path.join(__dirname,'items',last_name),path.join(__dirname,'items',name),(err)=>{
+    if (err) throw err
+    res.redirect('/');
+  }) 
+});
+
+
+
 
 //uploadBase
 
